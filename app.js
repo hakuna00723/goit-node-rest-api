b741a2c4a3249593f50395f9ec00e1507d3c0137
Serhii <110534352+hakuna00723@users.noreply.github.com>
2024-05-12 18:01:47 +0200

    summary work

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Database connection successful");
+      console.log(
+        `Server is running. Use our API on port: ${process.env.PORT}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log("Database connect unsuccessfully");
+    console.error(error.message);
+    process.exit(1);
+  });
 
 import contactsRouter from "./routes/contactsRouter.js";
 
@@ -19,8 +39,4 @@ app.use((_, res) => {
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
-});
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
 });
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,68 +1,29 @@
-import { promises as fs } from "fs";
-import path from "path";
-
-const contactsPath = path.resolve("db", "contacts.json");
+import { Contact } from "../schemas/contact.js";
 
 export async function listContacts() {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+  return Contact.find();
 }
 
 export async function getContactById(contactId) {
-  const data = await listContacts();
-
-  return data.filter((item) => item.id === contactId)[0] || null;
+  return Contact.findById(contactId);
 }
 
 export async function removeContact(contactId) {
-  const data = await listContacts();
-
-  const response = data.filter((item) => item.id === contactId)[0] || null;
-
-  const updatedData = data.filter((item) => item.id !== contactId) || null;
-
-  if (updatedData && updatedData.length > 0) {
-    await fs.writeFile(contactsPath, JSON.stringify(updatedData, null, 2));
-  }
-
-  return response;
+  return Contact.findByIdAndDelete(contactId);
 }
 
-export async function addContact(name, email, phone) {
-  const data = await listContacts();
-
-  const newUser = {
-    id: String(Date.now()),
-    name,
-    email,
-    phone,
-  };
-
-  data.push(newUser);
-
-  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
-
-  return newUser;
+export async function addContact(name, email, phone, favorite) {
+  return Contact.create({ name, email, phone, favorite });
 }
 
-export async function changeContact(id, name, email, phone) {
-  const contacts = await listContacts();
-  const contactID = contacts.findIndex((contact) => contact.id === id);
+export async function changeContact(contactID, name, email, phone, favorite) {
+  return Contact.findByIdAndUpdate(
+    contactID,
+    { contactID, name, email, phone, favorite },
+    { new: true }
+  );
+}
 
-  if (contactID === -1) {
-    return null;
-  }
-
-  const updatedContact = {
-    id,
-    name: name || contacts[contactID].name,
-    email: email || contacts[contactID].email,
-    phone: phone || contacts[contactID].phone,
-  };
-
-  contacts[contactID] = updatedContact;
-
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-
-  return updatedContact;
+export async function updateStatusContact(contactID, favorite) {
+  return Contact.findByIdAndUpdate(contactID, { favorite }, { new: true });
 }
